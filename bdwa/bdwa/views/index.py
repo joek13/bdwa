abc9f00f6a66c62ca36b8c9a4798ead76fd169e1
Joe Kerrigan <joek1301@gmail.com>
2020-09-12 13:16:10 -0400

    Sample template in index

--- bdwa/bdwa/views/index.py
+++ bdwa/bdwa/views/index.py
@@ -5,9 +5,16 @@ Defines the index view for the homepage.
 from django.http import HttpRequest, HttpResponse
 from django.template import loader
 
+from ..models import Listing
+
 def index_view(request: HttpRequest) -> HttpResponse:
     template  = loader.get_template("index.html")
 
-    context = None
+    listings = Listing.objects.all()
+    listings = [x.to_dict() for x in listings]
+
+    context = {
+        "listings": listings
+    }
 
     return HttpResponse(template.render(context, request))
