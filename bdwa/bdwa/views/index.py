021782c90d614c3aa5c882006e304e0fba04917c
Joe Kerrigan <joek1301@gmail.com>
2020-09-12 20:45:58 -0400

    Update show_listing; add splashes

--- bdwa/bdwa/views/index.py
+++ bdwa/bdwa/views/index.py
@@ -6,6 +6,15 @@ from django.http import HttpRequest, HttpResponse
 from django.template import loader
 
 from ..models import Listing
+import random
+
+SPLASHES = [
+    "Discover new music with mysterious crowdsourced descriptions.",
+    "Put someone on to your favorite album.",
+    "Less trauma than Tinder, or your money back.",
+    "Find your musical match.",
+    "Find your next big phase.",
+]
 
 def index_view(request: HttpRequest) -> HttpResponse:
     template  = loader.get_template("index2.html")
@@ -14,7 +23,8 @@ def index_view(request: HttpRequest) -> HttpResponse:
     listings = [x.to_dict() for x in listings]
 
     context = {
-        "listings": listings
+        "listings": listings,
+        "splash": random.choice(SPLASHES)
     }
 
     return HttpResponse(template.render(context, request))
